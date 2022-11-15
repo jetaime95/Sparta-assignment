4ee18a682f24644e0d586a11cc7254e4d877f784
seminju0172@gmail.com <seminju0172@gmail.com>
2022-11-15 09:46:26 +0900

    testcode

---
+++ 2022.11.15/users/tests.py
@@ -0,0 +1,16 @@
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+class UserRegistrationAPIViewTestCase(APITestCase):
+    def test_registration(self):
+        url = reverse('user_view')
+        user_data = {
+            "username" : "test",
+            "fullname" : "테스터",
+            "email" : "test@test.com",
+            "password" : "123",
+        }
+        response = self.client.post(url, user_data)
+        print(response.data)
+        self.assertEqual(response.data, {"message": "가입 완료!!"})
